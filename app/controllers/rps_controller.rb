5d0c82b1f461a416d32fd3b1cc8e077048685fb9
Ivanna <44304361+ivannarodriguez@users.noreply.github.com>
2023-10-29 01:54:47 +0000

    added methods

--- app/controllers/rps_controller.rb
+++ app/controllers/rps_controller.rb
@@ -1,1 +1,34 @@
-# class RpsController
+class RpsController < ApplicationController
+  def rock
+    @ours = "rock"
+    @theirs = ["rock", "paper", "scissors"].sample
+    @result = rock_paper_scissors(@ours, @theirs)
+  end
+
+  def paper
+    @ours = "paper"
+    @theirs = ["rock", "paper", "scissors"].sample
+    @result = rock_paper_scissors(@ours, @theirs)
+  end
+
+  def scissors
+    @ours = "scissors"
+    @theirs = ["rock", "paper", "scissors"].sample
+    @result = rock_paper_scissors(@ours, @theirs)
+  end
+
+  private
+
+  def rock_paper_scissors(ours, theirs)
+    result = case [ours, theirs]
+             when ["rock", "scissors"], ["paper", "rock"], ["scissors", "paper"]
+               "We won!"
+             when ["rock", "paper"], ["paper", "scissors"], ["scissors", "rock"]
+               "We lost!"
+             else
+               "We tied!"
+             end
+    return result
+  end
+
+end
