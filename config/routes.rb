8dd35cba25ec8cdd499f462aced54ec952777ef7
Ivanna <44304361+ivannarodriguez@users.noreply.github.com>
2023-10-29 01:27:01 +0000

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-
+  get("/") {:controler => "rps", :action => "rules"}
+  get("/rock") {:controler => "rps", :action =>}
+  get("/paper") {:controler => "rps", :action =>}
+  get("scissors") {:controler => "rps", :action =>}
 end
